9c0fa84ed235ba963368a3298224f488be9b1cd5
Sajjad Ali <sajjadalis@gmail.com>
2023-10-31 02:20:20 +0500

    removed capacitor, upgraded framework7 to v8 and all other packages

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -4,23 +4,40 @@
     <f7-navbar :sliding="false">
       <f7-nav-left>
         <f7-link panel-open="left">
-          <i class="i-mdi-menu w-7 h-7"></i>
+          <i class="i-mdi-menu w-5 h-5"></i>
         </f7-link>
       </f7-nav-left>
-      <f7-nav-title sliding>F7-Vue TypeScript</f7-nav-title>
+      <f7-nav-title
+        sliding
+        class="text-lg font-bold font-oswald uppercase text-blue-600 dark:text-white"
+        >F7-Vue TypeScript</f7-nav-title
+      >
       <f7-nav-right>
-        <f7-link panel-open="right">
-          <i class="i-mdi-menu w-7 h-7"></i>
-        </f7-link>
+        <div class="flex items-center">
+          <f7-link class="min-w-a px-2" @click="isDark = !isDark">
+            <i v-if="isDark" class="i-tabler-moon w-5 h-5"></i>
+            <i v-else class="i-tabler-sun w-5 h-5"></i>
+          </f7-link>
+          <f7-link class="min-w-a px-2" panel-open="right">
+            <i class="i-mdi-menu w-5 h-5"></i>
+          </f7-link>
+        </div>
       </f7-nav-right>
     </f7-navbar>
 
     <!-- Page content-->
-    <f7-block strong class="bg-blue-500 text-white mt-0">
+    <f7-block strong class="bg-blue-500 text-white mt-0 mb-0">
       <h2 class="text-xl font-bold mb-3">UnoCSS Classes & Icons</h2>
-      <p>This is an example of tabs-layout application. The main point of such tabbed layout is that each tab contains independent view with its own routing and navigation.</p>
+      <p>
+        This is an example of tabs-layout application. The main point of such
+        tabbed layout is that each tab contains independent view with its own
+        routing and navigation.
+      </p>
 
-      <p>Each tab/view may have different layout, different navbar type (dynamic, fixed or static) or without navbar like this tab.</p>
+      <p>
+        Each tab/view may have different layout, different navbar type (dynamic,
+        fixed or static) or without navbar like this tab.
+      </p>
 
       <div class="text-xs">
         <i class="i-mdi-alarm hover:text-teal-400 w-7 h-7" /> Material Icon
@@ -28,14 +45,11 @@
       <div class="text-xs mt-2">
         <i class="i-tabler-bell hover:text-teal-400 w-7 h-7" /> Tabler Icon
       </div>
-      <div class="text-xs mt-2">
-        <button class="i-carbon-sun dark:i-carbon-moon w-7 h-7 text-white" /> Carbon Icon (Sun for Light & Moon for Dark mode)
-      </div>
     </f7-block>
 
-    <f7-block strong>
+    <f7-block strong class="mt-0">
       <h2 text="xl blue-500" font="bold inter">Vue i18n Translation</h2>
-      <p>{{ $t('title') }}</p>
+      <p>{{ $t("title") }}</p>
 
       <f7-list no-hairlines-md class="p-0 -mx-4 mt-1 mb-0">
         <f7-list-input
@@ -45,37 +59,41 @@
           type="select"
           v-model:value="$i18n.locale"
         >
-          <option  v-for="locale in $i18n.availableLocales" :key="`locale-${locale}`" :value="locale">{{ locale }}</option>
+          <option
+            v-for="locale in $i18n.availableLocales"
+            :key="`locale-${locale}`"
+            :value="locale"
+          >
+            {{ locale }}
+          </option>
         </f7-list-input>
       </f7-list>
-      
     </f7-block>
-    
 
-    <f7-block-title>Navigation Buttons</f7-block-title>
-
-    <f7-block-title>Modals</f7-block-title>
+    <f7-block-title class="font-xl bold lobster">Modals</f7-block-title>
     <f7-block strong>
-      <f7-row>
-        <f7-col width="50">
+      <div class="grid grid-cols-2 grid-gap">
+        <div>
           <f7-button fill raised popup-open="#my-popup">Popup Screen</f7-button>
-        </f7-col>
-        <f7-col width="50">
-          <f7-button fill raised login-screen-open="#my-login-screen">Login Screen</f7-button>
-        </f7-col>
-      </f7-row>
+        </div>
+        <div>
+          <f7-button fill raised login-screen-open="#my-login-screen"
+            >Login Screen</f7-button
+          >
+        </div>
+      </div>
     </f7-block>
 
     <f7-block-title>Panels</f7-block-title>
     <f7-block strong>
-      <f7-row>
-        <f7-col width="50">
+      <div class="grid grid-cols-2 grid-gap">
+        <div>
           <f7-button fill raised panel-open="left">Left Panel</f7-button>
-        </f7-col>
-        <f7-col width="50">
+        </div>
+        <div>
           <f7-button fill raised panel-open="right">Right Panel</f7-button>
-        </f7-col>
-      </f7-row>
+        </div>
+      </div>
     </f7-block>
 
     <f7-list>
@@ -95,5 +113,33 @@
   </f7-page>
 </template>
 <script setup lang="ts">
-  import { f7Page, f7Navbar, f7NavRight, f7NavLeft, f7Block, f7List, f7ListItem, f7Row, f7Col, f7Link,  f7Button, f7NavTitle, f7BlockTitle, f7ListInput  } from 'framework7-vue';
+import {
+  f7,
+  f7Page,
+  f7Navbar,
+  f7NavRight,
+  f7NavLeft,
+  f7Block,
+  f7List,
+  f7ListItem,
+  f7Link,
+  f7Button,
+  f7NavTitle,
+  f7BlockTitle,
+  f7ListInput,
+} from "framework7-vue";
+import { ref, watch } from "vue";
+
+const isDark = ref(false);
+
+watch(
+  () => isDark.value,
+  async val => {
+    if (val === true) {
+      f7.setDarkMode(true);
+    } else {
+      f7.setDarkMode(false);
+    }
+  }
+);
 </script>
--- vite.config.ts
+++ vite.config.ts
@@ -1,48 +1,19 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
-import Unocss from 'unocss/vite'
-import { presetAttributify, presetUno } from 'unocss'
-import presetIcons from '@unocss/preset-icons'
-import presetWebFonts from '@unocss/preset-web-fonts'
+import Unocss from "unocss/vite";
+import { presetAttributify, presetUno } from "unocss";
+import presetIcons from "@unocss/preset-icons";
+import presetWebFonts from "@unocss/preset-web-fonts";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(), 
-    Unocss({
-      presets: [
-        // https://github.com/unocss/unocss/tree/main/packages/preset-attributify
-        presetAttributify({ /* preset options */}),
-        // https://github.com/unocss/unocss/tree/main/packages/preset-uno
-        presetUno(), 
-        // https://github.com/unocss/unocss/tree/main/packages/preset-icons/
-        presetIcons({
-          extraProperties: {
-            'display': 'inline-block',
-            'vertical-align': 'middle',
-          },
-        }),
-        presetWebFonts({
-          provider: 'google', // default provider
-          fonts: {
-            // these will extend the default theme
-            sans: 'Roboto',
-            mono: ['Fira Code', 'Fira Mono:400,700'],
-            // custom ones
-            lobster: 'Lobster',
-            inter: 'Inter',
-          },
-        }),
-        // ...custom presets
-      ]
-    })
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+  plugins: [vue(), Unocss()],
+  // resolve: {
+  //   alias: {
+  //     "@": fileURLToPath(new URL("./src", import.meta.url)),
+  //   },
+  // },
+});
