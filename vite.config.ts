43524b0893be0dd80abd5db5ce80f72f11ee4310
Sajjad Ali <sajjadalis@gmail.com>
2023-10-31 05:54:46 +0500

    added capacitor, fixed android issues, tested with android api 30/33

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,9 @@
-import { fileURLToPath, URL } from "node:url";
-
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
 import Unocss from "unocss/vite";
-import { presetAttributify, presetUno } from "unocss";
-import presetIcons from "@unocss/preset-icons";
-import presetWebFonts from "@unocss/preset-web-fonts";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), Unocss()],
-  // resolve: {
-  //   alias: {
-  //     "@": fileURLToPath(new URL("./src", import.meta.url)),
-  //   },
-  // },
 });
